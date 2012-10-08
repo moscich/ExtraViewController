7cbc05cc52df6a181734c75f6d576b0b545c5906
Marek Mościchowski <moscich@Mareks-Mac-mini-2.local>
2012-10-08 15:32:08 +0200

    Initial Commit

---
+++ testTests/testTests.h
@@ -0,0 +1,13 @@
+//
+//  testTests.h
+//  testTests
+//
+//  Created by Marek Mościchowski on 08.10.2012.
+//  Copyright (c) 2012 Marek Mościchowski. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testTests : SenTestCase
+
+@end
